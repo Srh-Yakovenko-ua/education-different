0569706fab5cb645095eba5d103bcf80936600aa
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-15 17:27:12 +0300

    different nesting options

--- React-douter-dom-v6/router-dom/src/App.tsx
+++ React-douter-dom-v6/router-dom/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from 'react-router-dom';
+import {NavLink, Outlet, Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
@@ -8,11 +8,44 @@ function App() {
             <NavLink to={'/'}>main</NavLink>
             <NavLink to={'/profile'}>Profile</NavLink>
             <NavLink to={'/login'}>Login</NavLink>
+            <NavLink to={'/profile/settings'}>settings</NavLink>
 
             <Routes>
                 <Route path={'/'} element={<div>main</div>}></Route>
-                <Route path={'/profile'} element={<div>Profile</div>}></Route>
+                <Route path={'/*'} element={<div>404</div>}></Route>
                 <Route path={'/login'} element={<div>Login</div>}></Route>
+                {/*<Route path={'/profile/*'} element={*/}
+                {/*    <div>*/}
+                {/*        Profile*/}
+                {/*        <Routes>*/}
+                {/*            <Route path={'/settings'} element={<div>settings</div>}></Route>*/}
+                {/*        </Routes>*/}
+                {/*    </div>*/}
+                {/*}>*/}
+                {/*</Route>*/}
+
+                {/*----------second nest---------------*/}
+                {/*<Route path={'/profile'} element={*/}
+                {/*    <div>*/}
+                {/*        Profile*/}
+                {/*        <Outlet/>*/}
+                {/*    </div>}>*/}
+                {/*    <Route path={'/profile/settings'} element={<div>settings</div>}></Route>*/}
+                {/*</Route>*/}
+
+                {/*----------third nest---------------*/}
+                {/*<Route path={'/profile'} element={*/}
+                {/*    <div>*/}
+                {/*        Profile*/}
+                {/*        <Outlet/>*/}
+                {/*    </div>}>*/}
+                {/*    <Route path={'*'} element={<div>profile page not found</div>}></Route>*/}
+                {/*    <Route index element={<div>check id</div>}></Route>*/}
+                {/*    <Route path={':id'} element={<div>profile page not found</div>}></Route>*/}
+                {/*    <Route path={'settings'} element={<div>settings</div>}></Route>*/}
+                {/*</Route>*/}
+
+
             </Routes>
 
         </div>

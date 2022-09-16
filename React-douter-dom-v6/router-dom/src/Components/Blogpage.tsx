020a1f5678e4056b1d978ff8204300f02a6b23cc
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-16 14:10:03 +0300

    add useEffect

--- React-douter-dom-v6/router-dom/src/Components/Blogpage.tsx
+++ React-douter-dom-v6/router-dom/src/Components/Blogpage.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Blogpage = () => {
+    const [posts, setPost] = useState<any>([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [])
+
     return (
         <div>
             <h1>Blog </h1>
             <p>this is demo website Blog</p>
+            {posts.map((post: {
+                title: string;
+                id: string }) =>{
+                return (
+                    <Link key={post.id} to={`/posts/${post.id}`}>
+                        <li>{post.title}</li>
+                    </Link>
+                )
+            })}
         </div>
     );
 };

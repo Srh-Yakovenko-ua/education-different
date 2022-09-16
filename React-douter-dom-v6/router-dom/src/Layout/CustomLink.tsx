bca4492d1b822378f2860bdf6abeca5b28fac5ab
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-16 14:09:51 +0300

    add useEffect

--- React-douter-dom-v6/router-dom/src/Layout/CustomLink.tsx
+++ React-douter-dom-v6/router-dom/src/Layout/CustomLink.tsx
@@ -3,14 +3,13 @@ import {Link, useMatch} from 'react-router-dom';
 
 const CustomLink = ({children,to,...restProps} : any ) => {
     const match = useMatch(to)
-    console.log(match)
+    console.log(match)  // match возвращает null или какой-то обьект если он есть
     return (
         <Link to={to}
               {...restProps}
             style={{
-                color : match ? 'blue' : 'white'
-            }}
-        >
+                color : match ? 'aqua' : 'white'
+            }}>
             {children}
         </Link>
     );

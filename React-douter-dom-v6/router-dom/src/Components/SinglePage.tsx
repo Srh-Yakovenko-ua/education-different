d9f319fc3cbdc39bb42eef9246087e61c0dd4e1e
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-16 14:17:14 +0300

    change singlePage

--- React-douter-dom-v6/router-dom/src/Components/SinglePage.tsx
+++ React-douter-dom-v6/router-dom/src/Components/SinglePage.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 
 const SinglePage = () => {
     const {id} = useParams()
-    console.log(useParams())
+    const [post, setPost] = useState<any>(null)
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => res.json())
+            .then(data => setPost(data))
+    }, [id])
+
 
     return (
         <div>
-            {id}
+            {post && (
+                <>
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                </>
+            )}
         </div>
     );
 };

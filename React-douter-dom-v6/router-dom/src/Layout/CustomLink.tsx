2cbed32e4ab51bd16965901c404df7425852ef4b
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-16 14:10:19 +0300

    Revert "add useEffect"
    This reverts commit bca4492d1b822378f2860bdf6abeca5b28fac5ab.

--- React-douter-dom-v6/router-dom/src/Layout/CustomLink.tsx
+++ React-douter-dom-v6/router-dom/src/Layout/CustomLink.tsx
@@ -3,13 +3,14 @@ import {Link, useMatch} from 'react-router-dom';
 
 const CustomLink = ({children,to,...restProps} : any ) => {
     const match = useMatch(to)
-    console.log(match)  // match возвращает null или какой-то обьект если он есть
+    console.log(match)
     return (
         <Link to={to}
               {...restProps}
             style={{
-                color : match ? 'aqua' : 'white'
-            }}>
+                color : match ? 'blue' : 'white'
+            }}
+        >
             {children}
         </Link>
     );

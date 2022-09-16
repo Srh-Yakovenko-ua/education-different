18f7d1e1fc51f9ce3078ce48cc862666da99e1b7
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-16 16:31:59 +0300

    useSearchParams

--- React-douter-dom-v6/router-dom/src/Components/Blogpage.tsx
+++ React-douter-dom-v6/router-dom/src/Components/Blogpage.tsx
@@ -1,9 +1,30 @@
 import React, {useEffect, useState} from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import {Link, useLocation, useSearchParams} from 'react-router-dom';
+import {isBooleanObject} from 'util/types';
 
 const Blogpage = () => {
     const [posts, setPost] = useState<any>([])
     console.log(useLocation())
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const postQuery = searchParams.get('post') || '';
+    const latest = searchParams.has('latest');
+
+    const startsFrom = latest ? 80 : 1;
+
+    const handleSubmit = (e: { target: any; preventDefault: () => void; }) => {
+        e.preventDefault()
+        const form = e.target
+
+        const query = form.search.value;
+        const isLatest = form.latest.checked
+
+        const params : any = {}
+        if(query.length) params.post = query
+        if(isLatest) params.latest = true
+        setSearchParams(params)
+    }
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
@@ -13,9 +34,18 @@ const Blogpage = () => {
     return (
         <div>
             <h1>Blog </h1>
-            <p>this is demo website Blog</p>
+            <form autoComplete={'off'} onSubmit={handleSubmit}>
+                <input type="search" name={'search'}/>
+                <label htmlFor="">
+                    <input type="checkbox" name={'latest'}/>
+                </label>
+                <input type="submit" value={'Search'}/>
+            </form>
+
             <Link to={'/posts/new'}>Add new post</Link>
-            {posts.map((post: {
+            {posts.filter((post: {
+                id: number;
+                title: string | string[]; }) => post.title.includes(postQuery) && post.id >= startsFrom).map((post: {
                 title: string;
                 id: string
             }) => {
